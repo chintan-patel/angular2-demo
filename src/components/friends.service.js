f3ec96db1b37a385c7857aeaf49406bc34855cab
Chintan Patel <patelchintanm@gmail.com>
2015-10-06 13:27:40 -0400

    - Adding version 1 for demo on Angular 37 alpha

---
+++ src/components/friends.service.js
@@ -0,0 +1,7 @@
+var FriendsService = (function () {
+    function FriendsService() {
+        this.names = ['Chintan', 'Patel'];
+    }
+    return FriendsService;
+})();
+//# sourceMappingURL=friends.service.js.map
---
+++ src/components/friends.service.ts
@@ -0,0 +1,7 @@
+class FriendsService {
+  names:Array<string>;
+
+  constructor(){
+    this.names = ['Chintan', 'Patel'];
+  }
+}
